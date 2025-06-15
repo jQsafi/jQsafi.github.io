e5d7b161826db93b07f405cc773122cded31c81f
Shafayat <shafayat@engineer.com>
2025-06-15 16:59:20 +0600

    Force add build folder for deployment

--- html/3d-css-box/README.txt
+++
@@ -1,4 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/jQsafi/pen/ZqQgBz.
-
- 
-Rotating 3D CSS BOX
--- html/js/app.js
+++
@@ -1,7 +0,0 @@
-var $_GET=[];
-window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(a,name,value){$_GET[name]=value;});
-if($_GET['page'])
-{
-	$("#container").attr("w3-include-html",$_GET['page']+"/index.html");
-	w3.includeHTML();
-}
